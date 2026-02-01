09f0c8184bc1c2cd6b33043e06f90eb7d8fe627d
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-02 00:04:30 +0900

    新規作成画面

--- myproject/diary/models.py
+++ myproject/diary/models.py
@@ -5,7 +5,7 @@ class Page(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID")
     title = models.CharField(max_length=100, verbose_name="タイトル")
     body = models.TextField(max_length=2000, verbose_name="本文")
-    page_data = models.DateField(verbose_name="日付")
+    page_date = models.DateField(verbose_name="日付")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
 
