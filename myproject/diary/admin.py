6c177d8659007cc08b7f366c3c2f6d16925eb88f
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-02 00:13:55 +0900

    変更できないものも表示させる

--- myproject/diary/admin.py
+++ myproject/diary/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Page
 
-admin.site.register(Page)
+# デフォルトは変更できないデータは見れもしないようになっていたのを表示させる
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ["id", "created_at", "updated_at"]
